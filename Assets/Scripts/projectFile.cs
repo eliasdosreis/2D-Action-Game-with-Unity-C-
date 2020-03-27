b2c8bb55446aac82dd32d07ac1d4dbc3af9f9f9d
Elias.Lima <elias_dos_reis@hotmail.com>
2020-03-27 09:37:52 -0300

    Corrigindo Bug Git

--- Assets/Scripts/projectFile.cs
+++ Assets/Scripts/projectFile.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class projectFile : MonoBehaviour {
 	public float speed;
 	public float lifeTime;
-
-
-
+	public GameObject explosion;
 
 	// Use this for initialization
 	void Start () {
-		Destroy (gameObject, lifeTime);
+		//Destroy (gameObject, lifeTime);
+		Invoke ("DestroyProjectile", lifeTime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector2.up * -speed * Time.deltaTime);
+	}
+
+	void DestroyProjectile(){
+		Instantiate (explosion, transform.position, Quaternion.identity);
+		Destroy (gameObject);
 	}
 }
