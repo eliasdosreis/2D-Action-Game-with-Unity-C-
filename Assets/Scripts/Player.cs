6d079f61d97ef69c506b01da80320e6ce4563fab
Elias.Lima <elias_dos_reis@hotmail.com>
2020-03-27 07:46:00 -0300

    Player + Animação + Movimentação + Tiro

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	private Animator anim;
+	private Rigidbody2D rigid;
+
+	[SerializeField]
+	private Vector2 moveAmount;
+
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update(){
+		moveAmount = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+
+		if (moveAmount != Vector2.zero) {
+			anim.SetBool ("isRunning", true);
+		} else {
+			anim.SetBool ("isRunning", false);
+		}
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		Vector2 inputMove = moveAmount.normalized * speed;
+
+		rigid.MovePosition (rigid.position + inputMove * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/projectFile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectFile : MonoBehaviour {
+	public float speed;
+	public float lifeTime;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector2.up * -speed * Time.deltaTime);
+	}
+}
